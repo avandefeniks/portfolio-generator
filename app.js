8c45a24d0056666632dd537b712f16d3064dfc6e
avandefeniks <angel.vandefeniks@gmail.com>
2022-05-10 18:14:04 -0700

    added the ability to create an html file. modulerized the code into two seperate files

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);   
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
-const printProfileData = profileDataArr => {
-    // this...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
-    
-    console.log('==================');
+const profileDataArgs = process.argv.slice(2);
 
-    // is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+const [name, github] = profileDataArgs;
 
-printProfileData(profileDataArgs);
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
+
+    console.log('portfolio complete! Check out index.html to see the output!');
+});
